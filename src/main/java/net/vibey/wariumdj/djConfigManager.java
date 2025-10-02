c6b71f499a211d6d6164cb1bc3b681eecac17940
KindaVibey <joshuaspirek@gmail.com>
2025-10-01 20:43:59 -0400

    stuff

--- src/main/java/net/vibey/wariumdj/djConfigManager.java
+++ src/main/java/net/vibey/wariumdj/djConfigManager.java
@@ -16,7 +16,7 @@ import java.util.Set;
 public class djConfigManager {
 
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-    private static final File CONFIG_FILE = new File("config/soundtweaker.json");
+    private static final File CONFIG_FILE = new File("config/djConfig.json");
     private static final Map<String, Float> volumeMap = new HashMap<>();
 
     public static void load() {
